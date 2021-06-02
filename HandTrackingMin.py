5960be68db976ac17fe845c114e31a4f7ed58596
RbChoi <rayun.rb.choi@gmail.com>
2021-06-02 16:55:34 +0900

    :sparkles: 기본적인 Hand Tracking을 위한 opencv 코드

--- HandTrackingMin.py
+++ HandTrackingMin.py
@@ -6,7 +6,7 @@ cap = cv2.VideoCapture(0)
 
 mpHands = mp.solutions.hands
 hands = mpHands.Hands()
-mpDraw = mp.solutions.drawing_utils
+mpDraw = mp.solutions.drawing_utils #손을 그리기 위한 작
 
 pTime = 0
 cTime = 0
@@ -16,10 +16,15 @@ while True:
     imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     results = hands.process(imgRGB)
 
+    #result 확인 위해 출력
+    #print(results)
+    #출력 결과는 "<class 'mediapipe.python.solution_base.SolutionOutputs'>
     #print(results.multi_hand_landmarks)
+    #변화있으면 x,y,z 출력 없으면 None
 
     if results.multi_hand_landmarks:
         for handLms in results.multi_hand_landmarks:
+
             for id, lm in enumerate(handLms.landmark):
                 #print(id, lm)
                 h, w, c=img.shape
@@ -28,13 +33,16 @@ while True:
 
                 cv2.circle(img, (cx, cy), 15, (255, 0, 255), cv2.FILLED)
 
+            #읽어온 img를 손 position, 손 연결 그린다.
             mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
 
+    #현재 시간 읽어오
     cTime = time.time()
+    #초당 프레임 수 계
     fps = 1 / (cTime - pTime)
     pTime = cTime
 
-
+    #img 저장하고 fps를 string으로 변환하기
     cv2.putText(img, str(fps), (30, 70), cv2.FONT_HERSHEY_PLAIN, 3,(255, 0, 255), 3)
 
     cv2.imshow("Image", img)
@@ -43,4 +51,6 @@ while True:
 
     # ESC 키누르면 종료
     #if cv2.waitKey(1) & 0xFF == 27:
-     #  break
+    #  break
+    cap.release()
+    cv2.destroyAllWindows()
